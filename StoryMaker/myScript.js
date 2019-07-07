b17210143f35e658e52623ffaa22b9e1c008d05d
Kamrul Hasan <52586864+kamrulcsepstu@users.noreply.github.com>
2019-07-07 12:25:04 +0600

    Add files via upload

---
+++ StoryMaker/myScript.js
@@ -0,0 +1,6 @@
+var adjective = prompt('Please type an adjective');
+var verb = prompt('Please type a verb');
+var noun = prompt('Please type a noun');
+alert('All done.Ready for the message?');
+
+document.write('There was a ' + adjective + ' programmer who wanted to ' + verb + ' the ' + noun + '.' );
